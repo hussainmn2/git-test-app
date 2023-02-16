a5c301f0a5a91ad3a183e596d6e624e3429316cb
Tabrez Hussain <hussainmn2@gmail.com>
2023-02-16 16:24:39 +0530

    added dto for method argument in controller class

--- src/main/java/com/hsn/controller/GitController.java
+++ src/main/java/com/hsn/controller/GitController.java
@@ -3,23 +3,27 @@
  */
 package com.hsn.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hsn.dto.Order;
+
 /**
- * @author hussa
+ * @author hussain
  *
  */
 
 @RestController
+@RequestMapping("/getOrder")
 public class GitController {
 
 	@PostMapping("/purchase/{userName}/{amount}/{productName}")
-	public String purchase(@PathVariable String userName, @PathVariable double amount,
-			@PathVariable String productName) {
+	public String purchase(@RequestBody Order order) {
 
-		return "Hi " + userName + "Order for" +productName + "with amount" + amount+ "Stored Successfully" ;
+		return "Hi " + order.getUserName() + "Order for" + order.getProductName() + "with amount" + order.getAmount()
+				+ "Stored Successfully";
 
 	}
 
---
+++ src/main/java/com/hsn/dto/Order.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.hsn.dto;
+
+import lombok.Data;
+
+/**
+ * @author hussa
+ *
+ */
+@Data
+public class Order {
+	
+	private String userName;
+	private Double amount;
+	private String productName;
+
+}
